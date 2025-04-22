5a69b93d92ca814692c301f6da34b364a3a3d6de
David Pratama <76859181+daviddprtma@users.noreply.github.com>
2025-04-22 12:41:30 +0000

    done for all

---
+++ target/debug/deps/dummy_rust_project-0fd2957682ffc735.d
@@ -0,0 +1,5 @@
+/workspaces/dummy-rust-project/target/debug/deps/dummy_rust_project-0fd2957682ffc735: src/main.rs
+
+/workspaces/dummy-rust-project/target/debug/deps/dummy_rust_project-0fd2957682ffc735.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/dummy_rust_project-2bdd86ae7436ae78.d
@@ -0,0 +1,5 @@
+/workspaces/dummy-rust-project/target/debug/deps/dummy_rust_project-2bdd86ae7436ae78: src/lib.rs
+
+/workspaces/dummy-rust-project/target/debug/deps/dummy_rust_project-2bdd86ae7436ae78.d: src/lib.rs
+
+src/lib.rs:
